bcf4b6cf9214fa76743a7285b354dbc0b8250d78
utkarshjain45 <jainutkarsh096@gmail.com>
2025-05-01 09:14:28 +0530

    solution 2 for 503

--- prblm503.java
+++ prblm503.java
@@ -2,6 +2,7 @@ import java.util.Stack;
 
 public class prblm503 {
 
+    //Solution 1
     public static int[] nextGreaterElements(int[] nums) {
        int[] tempArr = new int[nums.length * 2];
        for (int i = 0; i < nums.length; i++) {
@@ -31,11 +32,45 @@ public class prblm503 {
         }
         return ans;
     }
+
+    //Solution 2
+    public int[] nextGreaterElements2(int[] nums) {
+        int[] ans = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+
+        // precomputations
+        for(int i = nums.length - 1; i >= 0; i--) {
+            int curr = nums[i];
+            while(!stack.isEmpty() && stack.peek() <= curr) {
+                stack.pop();
+            }
+            stack.push(curr);
+        }
+
+        for(int i = nums.length - 1; i >= 0; i--) {
+            int curr = nums[i];
+            while(!stack.isEmpty() && stack.peek() <= curr) {
+                stack.pop();
+            }
+            if(stack.isEmpty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = stack.peek();
+            }
+            stack.push(curr);
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int[] nums = {1,2,1};
         int[] answer = nextGreaterElements(nums);
+        int[] answer2 = nextGreaterElements(nums);
         for (int i = 0; i < answer.length; i++) {
             System.out.print(answer[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < answer2.length; i++) {
+            System.out.print(answer2[i] + " ");
         }
     }
 }
