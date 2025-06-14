69c143858298321ecb33e09d0b12e904f87f62e9
utkarshjain45 <jainutkarsh096@gmail.com>
2025-06-14 20:42:14 +0530

    Set Matrix Zeroes

---
+++ prblm73.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+public class prblm73 {
+    public static void main(String[] args) {
+        int[][] matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        setZeroes(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        int col = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+
+                    if(j != 0){
+                        matrix[0][j] = 0;
+                    }
+                    else{
+                        col = 0;
+                    }
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (col == 0) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+
+    public static void setZeroesBruteForce1(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0){
+                    for (int j2 = 0; j2 < matrix.length; j2++) {
+                        if (matrix[j2][j] != 0) {
+                            matrix[j2][j] = -2;
+                        }
+                    }
+                    for (int j2 = 0; j2 < matrix[0].length; j2++) {
+                        if (matrix[i][j2] != 0) {
+                            matrix[i][j2] = -2;
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == -2) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+
+    public static void setZeroesBruteForce2(int[][] matrix) {
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
