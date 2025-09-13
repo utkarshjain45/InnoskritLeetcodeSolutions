87cd31788ad6ee0e2e6ca699a57eb7377856f7ec
utkarshjain45 <jainutkarsh096@gmail.com>
2025-09-13 10:49:26 +0530

    Find Peak Element

---
+++ prblm162.java
@@ -0,0 +1,21 @@
+public class prblm162 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(new prblm162().findPeakElement(nums));
+    }
+
+    public int findPeakElement(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid+1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
