97f4adebd0e2363df7034745ec1aa185a2e55b72
utkarshjain45 <jainutkarsh096@gmail.com>
2025-10-03 00:15:42 +0530

    Compare Version Numbers

---
+++ prblm165.java
@@ -0,0 +1,22 @@
+public class prblm165 {
+    public static void main(String[] args) {
+        String version1 = "1.2", version2 = "1.10";
+        System.out.println(new prblm165().compareVersion(version1, version2));   
+    }
+
+    public int compareVersion(String version1, String version2) {
+        for (int i = 0, j = 0; i < version1.length() || j < version2.length(); i++, j++) {
+            int a = 0, b = 0;
+            while (i < version1.length() && version1.charAt(i) != '.') {
+                a = (a * 10) + (version1.charAt(i++) - '0');
+            }
+            while (j < version2.length() && version2.charAt(j) != '.') {
+                b = (b * 10) + (version2.charAt(j++) - '0');
+            }
+            if(a != b){
+                return a < b ? -1 : 1; 
+            }
+        }
+        return 0;
+    }
+}
