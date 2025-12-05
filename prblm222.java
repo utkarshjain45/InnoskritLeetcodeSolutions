9caf928e602cf432701eac18ae59eed34162fd45
utkarshjain45 <jainutkarsh096@gmail.com>
2025-12-05 23:04:42 +0530

    Count Complete Tree Nodes

---
+++ prblm222.java
@@ -0,0 +1,40 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {}
+    public TreeNode(int val){
+        this.val = val;
+    }
+    public TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class prblm222 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        System.out.println(new prblm222().countNodes(root));        
+    }
+
+    int count;
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+        count++;
+        if(root.left != null){
+            countNodes(root.left);
+        }
+        if(root.right != null){
+            countNodes(root.right);
+        }
+        return count;
+    }
+}
