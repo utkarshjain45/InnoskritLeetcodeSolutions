72e861163797b00663f1a604c0217ff49fd181bb
utkarshjain45 <jainutkarsh096@gmail.com>
2025-12-05 23:05:05 +0530

    Same Tree

---
+++ prblm100.java
@@ -0,0 +1,31 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class prblm100 {
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(3);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(5);
+        root2.right = new TreeNode(3);
+        System.out.println(new prblm100().isSameTree(root1, root2));        
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
